a3ca5367b8957178626f5b2fe4c6444ebc1e4ada
dejan anaconda <dejan anaconda>
2016-09-07 18:18:18 +0200

    add particles.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
 console.log('\'Allo \'Allo!');
+
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/bower_components/particles.js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
