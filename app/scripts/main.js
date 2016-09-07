2b3f7ac12f139b00efa526ca82364f812dcad5a4
dejan anaconda <dejan anaconda>
2016-09-07 19:22:02 +0200

    header background color gradient

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,3 +5,38 @@ console.log('\'Allo \'Allo!');
 particlesJS.load('particles-js', '/bower_components/particles.js/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+
+$(document).ready(function () {
+
+    $("nav ul li a").click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 60
+        }, 1200)
+        ;
+        event.preventDefault();
+        return false;
+    });
+
+    $("#moveDown").click(function () {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 0
+        }, 1200)
+        ;
+        event.preventDefault();
+        return false;
+    });
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var welcome = $('#welcome').height();
+        if (scroll >= welcome) {
+            $("nav").addClass("header-fixed", 600);
+        }
+        else {
+            $("nav").removeClass("header-fixed", 600);
+        }
+
+    });
+
+});
