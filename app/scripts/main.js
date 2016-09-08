97b37c76648bd0f34b9d73791a692b1cfd8b2a83
mladjone <mladjone@gmail.com>
2016-09-08 09:56:40 +0200

    main.js changed function particlesJS, from calling external .json file to internal function parametar calling.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,14 +2,120 @@ console.log('\'Allo \'Allo!');
 
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/bower_components/particles.js/particles.json', function() {
-    console.log('callback - particles.js config loaded');
+particlesJS('particles-js', {
+  'particles': {
+    'number': {
+      'value': 15,
+      'density': {
+        'enable': false,
+        'value_area': 6707.754953874318
+      }
+    },
+    'color': {
+      'value': '#1be8c4'
+    },
+    'shape': {
+      'type': 'circle',
+      'stroke': {
+        'width': 0,
+        'color': '#000000'
+      },
+      'polygon': {
+        'nb_sides': 8
+      },
+      'image': {
+        'src': 'img/github.svg',
+        'width': 100,
+        'height': 100
+      }
+    },
+    'opacity': {
+      'value': 0.6493818846505367,
+      'random': false,
+      'anim': {
+        'enable': false,
+        'speed': 1,
+        'opacity_min': 0.1,
+        'sync': false
+      }
+    },
+    'size': {
+      'value': 4.008530152163807,
+      'random': true,
+      'anim': {
+        'enable': false,
+        'speed': 40,
+        'size_min': 0.1,
+        'sync': false
+      }
+    },
+    'line_linked': {
+      'enable': true,
+      'distance': 16.03412060865523,
+      'color': '#ffffff',
+      'opacity': 0.04008530152163807,
+      'width': 0
+    },
+    'move': {
+      'enable': true,
+      'speed': 1.603412060865523,
+      'direction': 'none',
+      'random': false,
+      'straight': false,
+      'out_mode': 'out',
+      'bounce': false,
+      'attract': {
+        'enable': false,
+        'rotateX': 600,
+        'rotateY': 1200
+      }
+    }
+  },
+  'interactivity': {
+    'detect_on': 'canvas',
+    'events': {
+      'onhover': {
+        'enable': false,
+        'mode': 'bubble'
+      },
+      'onclick': {
+        'enable': true,
+        'mode': 'push'
+      },
+      'resize': true
+    },
+    'modes': {
+      'grab': {
+        'distance': 400,
+        'line_linked': {
+          'opacity': 1
+        }
+      },
+      'bubble': {
+        'distance': 400,
+        'size': 40,
+        'duration': 2,
+        'opacity': 8,
+        'speed': 3
+      },
+      'repulse': {
+        'distance': 200,
+        'duration': 0.4
+      },
+      'push': {
+        'particles_nb': 4
+      },
+      'remove': {
+        'particles_nb': 2
+      }
+    }
+  },
+  'retina_detect': true
 });
-
 
 $(document).ready(function () {
 
-    $("nav ul li a").click(function () {
+    $('nav ul li a').click(function () {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top - 51
         }, 1200)
@@ -18,7 +124,7 @@ $(document).ready(function () {
         return false;
     });
 
-    $("#moveDown").click(function () {
+    $('#moveDown').click(function () {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top - 0
         }, 1200)
@@ -31,28 +137,28 @@ $(document).ready(function () {
         var scroll = $(window).scrollTop();
         var welcome = $('#welcome').height();
         if (scroll >= welcome) {
-            $("nav").addClass("header-fixed", 600);
+            $('nav').addClass('header-fixed', 600);
         }
         else {
-            $("nav").removeClass("header-fixed", 600);
+            $('nav').removeClass('header-fixed', 600);
         }
     });
 
-    $("span.glyphicon-align-justify").click(function () {
+    $('span.glyphicon-align-justify').click(function () {
         event.preventDefault();
-        $("nav.mobile").show(300);
-         $("span.glyphicon-align-justify").hide(300);
+        $('nav.mobile').show(300);
+         $('span.glyphicon-align-justify').hide(300);
     });
 
-    $("span.glyphicon-remove").click(function () {
+    $('span.glyphicon-remove').click(function () {
         event.preventDefault();
-        $("nav.mobile").hide(300);
-        $("span.glyphicon-align-justify").show(300);
+        $('nav.mobile').hide(300);
+        $('span.glyphicon-align-justify').show(300);
      });
-    $("nav.mobile a").click(function () {
+    $('nav.mobile a').click(function () {
         event.preventDefault();
-        $("nav.mobile").hide(300);
-        $("span.glyphicon-align-justify").show(300);
+        $('nav.mobile').hide(300);
+        $('span.glyphicon-align-justify').show(300);
     });
 
 });
