00e482d52137b929e0cb2ca0177c620b87a60fba
dejan anaconda <dejan anaconda>
2016-09-12 12:19:06 +0200

    no message

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -132,6 +132,7 @@ $(document).ready(function () {
         event.preventDefault();
         return false;
     });
+
     $('#moveUp').click(function () {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top - 0
@@ -151,8 +152,6 @@ $(document).ready(function () {
             $('nav').removeClass('header-fixed', 600);
         }
     });
-
-
 
     $(window).on('scroll', function () {
         var logo = $('.logo');
